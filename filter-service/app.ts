7d5fea7e06ed77c1132a45eb9e653d31a72e95e3
Andersonlima13 <anderson.lima18pb@gmail.com>
2026-04-02 21:29:43 -0300

    filtrar filme

--- filter-service/app.ts
+++ filter-service/app.ts
@@ -2,11 +2,12 @@ import { FilterConsumer } from './services/FilterConsumer.js'
 import { VideoFilter }    from './services/VideoFilter.js'
 
 const filter = new VideoFilter({
-  minLikes:    0,
-  minViews:    100,
-  minComments: 50,
+  minLikes:    50000,
+  minViews:    0,
+  minComments: 0,
   minShares:   0,
-
+  requireCaption: true, 
+  captionMustContain: ['filme', 'series','filmetop' , 'melhorserie', 'filmes', 'filme' ,'ftv'] 
 })
 
 const consumer = new FilterConsumer(filter)
